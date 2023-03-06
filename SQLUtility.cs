ec4894db71568d1af7e21ad64a2a784b647d9de3
S Breuer <support@app100.co.uk>
2023-03-06 13:44:57 +0000

    initial commit

---
+++ SQLUtility.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace CPUFrameWork
+{
+    public class SQLUtility
+    {
+        public static string ConnectionString = "";
+        public static DataTable GetDataTable(string sqlcmd)
+        {
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+
+            SqlCommand cmd = new();
+            cmd.CommandText = sqlcmd;
+            cmd.Connection = conn;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
