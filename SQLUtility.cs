8cb547f091896e2b1ab284098d539bf1c2f93c75
S Breuer <support@app100.co.uk>
2023-05-29 19:19:42 -0400

    Exception handling

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
+using System.Text;
 
 namespace CPUFrameWork
 {
@@ -24,17 +25,25 @@ namespace CPUFrameWork
 
         public static DataTable GetDataTable(SqlCommand cmd)
         {
-            Debug.Print("-----" + Environment.NewLine + cmd.CommandText);
-
             DataTable dt = new DataTable();
             using (SqlConnection conn = new(ConnectionString))
             {
                 conn.Open();
                 cmd.Connection = conn;
-                SqlDataReader dr = cmd.ExecuteReader();
-                dt.Load(dr);
-                SetAllColumnsAllowNull(dt);
+                Debug.Print(GetSql(cmd));
+                try
+                {
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    dt.Load(dr);
+                }
+                catch (SqlException ex)
+                {
+                    string msg = ParseConstraintMsg(ex.Message);
+                    throw new Exception(msg);
+                }
             }
+            SetAllColumnsAllowNull(dt);
             return dt;
         }
         public static DataTable GetDataTable(string sqlcmd)
@@ -47,6 +56,43 @@ namespace CPUFrameWork
             GetDataTable(sqlcmd);
         }
 
+        private static string ParseConstraintMsg(string msg)
+        {
+            string origmsg = msg;
+            string prefix = "ck_";
+            string msgend = "";
+            if (!msg.Contains(prefix))
+            {
+                if (msg.Contains("u_"))
+                {
+                    prefix = "u_";
+                    msgend = " must be unique";
+                }
+                else if (msg.Contains("f_"))
+                {
+                    prefix = "f_";
+                }
+            }
+            if (msg.Contains(prefix))
+            {
+                msg = msg.Replace("\"", "'");
+                int pos = msg.IndexOf(prefix) + prefix.Length;
+                msg = msg.Substring(pos);
+                pos = msg.IndexOf("'");
+                if (pos == -1)
+                {
+                    msg = origmsg;
+                }
+                else
+                {
+
+                    msg = msg.Substring(0, pos);
+                    msg = msg.Replace("_", " ");
+                    msg += msgend;
+                }
+            }
+            return msg;
+        }
         public static int GetFirstColunmFirstRowValue(string sql)
         {
 
@@ -74,6 +120,45 @@ namespace CPUFrameWork
                 c.AllowDBNull = true;
             }
         }
+
+        public static string GetSql(SqlCommand cmd)
+        {
+            string val = "";
+#if DEBUG
+            StringBuilder sb = new StringBuilder();
+
+            if (cmd.Connection != null)
+            {
+                sb.AppendLine($"-- {cmd.Connection.DataSource}");
+                sb.AppendLine($"use {cmd.Connection.Database}");
+                sb.AppendLine("GO");
+            }
+            if (cmd.CommandType == CommandType.StoredProcedure)
+            {
+                sb.AppendLine($"exec {cmd.CommandText}");
+                int paramcount = cmd.Parameters.Count - 1;
+                int paramnum = 0;
+                string comma = ",";
+                foreach (SqlParameter p in cmd.Parameters)
+                {
+                    if (p.Direction != ParameterDirection.ReturnValue)
+                    {
+                        if (paramnum == paramcount) comma = "";
+                        sb.AppendLine($"{p.ParameterName} = {(p.Value == null ? "null" : p.Value.ToString())}{comma}");
+
+                    }
+                    paramnum++;
+                }
+            }
+            else
+            {
+                sb.AppendLine($"{cmd.CommandText}");
+            }
+            val = sb.ToString();
+#endif
+            return val;
+        }
+
         public static void DebugPrintDataTable(DataTable dt)
         {
             foreach (DataRow r in dt.Rows)
